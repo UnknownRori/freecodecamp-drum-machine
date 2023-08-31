39acfae5f8ba33899f31fe08d9f5c727325e793c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-31 13:33:14 +0700

    🚀 Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="flex flex-col gap-4 justify-center items-center min-h-screen w-screen">
+      <h2 className="text-red-500">Hello, World!</h2>
+    </main>
+  );
+}
+
+export default App
